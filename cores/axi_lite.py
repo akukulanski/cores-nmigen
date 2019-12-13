07a7f70bfa89ae03234bcfa14dc25d6d4fb3d2f7
akukulanski <ariel.kukulanski@satellogic.com>
2019-12-13 13:11:04 -0300

    axi lite: refactor

--- cores/axi_lite.py
+++ cores/axi_lite.py
@@ -22,10 +22,28 @@ class AxiLiteDevice(Elaboratable):
         sync = m.d.sync
         comb = m.d.comb
 
-        # Axi Lite Slave Interface
+        # Registers
 
+        we = Signal()
         wr_addr = Signal(self.addr_w)
         wr_data = Signal(self.data_w)
+
+        with m.If(self.axi_lite.aw_accepted()):
+            sync += wr_addr.eq(self.axi_lite.awaddr)
+
+        with m.If(self.axi_lite.w_accepted()):
+            sync += wr_data.eq(self.axi_lite.wdata)
+        
+        for reg_name, reg_dir, reg_addr in self._regs:
+            if reg_dir == 'rw':
+                with m.If((wr_addr == reg_addr) & (we == 1)):
+                    sync += getattr(self.registers, reg_name).eq(wr_data)
+
+        for reg_name, reg_dir, reg_addr in self._regs:
+            with m.If(self.axi_lite.ar_accepted() & (self.axi_lite.araddr == reg_addr)):
+                sync += self.axi_lite.rdata.eq(getattr(self.registers, reg_name))
+
+        # Axi Lite Slave Interface
         
         comb += self.axi_lite.bresp.eq(0)
         comb += self.axi_lite.rresp.eq(0)
@@ -35,14 +53,6 @@ class AxiLiteDevice(Elaboratable):
                 comb += self.axi_lite.arready.eq(1)
                 comb += self.axi_lite.rvalid.eq(0)
                 with m.If(self.axi_lite.ar_accepted()):
-                    #########################################################
-                    # HOW CAN I COLLAPSE THIS INTO A FUNCION?
-                    #sync += ...
-                    for r in self._regs:
-                        reg_name, reg_dir, reg_addr = r
-                        with m.If(self.axi_lite.araddr == reg_addr):
-                            sync += self.axi_lite.rdata.eq(getattr(self.registers, reg_name))
-                    #########################################################
                     m.next = "READ"
             with m.State("READ"):
                 comb += self.axi_lite.arready.eq(0)
@@ -55,58 +65,33 @@ class AxiLiteDevice(Elaboratable):
             with m.State("IDLE"):
                 comb += [self.axi_lite.awready.eq(1),
                          self.axi_lite.wready.eq(1),
-                         self.axi_lite.bvalid.eq(0),]
+                         self.axi_lite.bvalid.eq(0),
+                         we.eq(0),]
                 with m.If(self.axi_lite.aw_accepted() & self.axi_lite.w_accepted()):
-                    #########################################################
-                    # HOW CAN I COLLAPSE THIS INTO A FUNCION?
-                    #sync += self.write_reg(self.axi_lite.awaddr, self.axi_lite.wdata, m)
-                    for r in self._regs:
-                        reg_name, reg_dir, reg_addr = r
-                        if reg_dir == 'rw':
-                            with m.If(self.axi_lite.awaddr == reg_addr):
-                                sync += getattr(self.registers, reg_name).eq(self.axi_lite.wdata)
-                    #########################################################
                     m.next = "DONE"
                 with m.Elif(self.axi_lite.aw_accepted()):
-                    sync += wr_addr.eq(self.axi_lite.awaddr)
                     m.next = "WAITING_DATA"
                 with m.Elif(self.axi_lite.w_accepted()):
-                    sync += wr_data.eq(self.axi_lite.wdata)
                     m.next = "WAITING_ADDR"
             with m.State("WAITING_DATA"):
                 comb += [self.axi_lite.awready.eq(0),
                          self.axi_lite.wready.eq(1),
-                         self.axi_lite.bvalid.eq(0),]
+                         self.axi_lite.bvalid.eq(0),
+                         we.eq(0),]
                 with m.If(self.axi_lite.w_accepted()):
-                    #########################################################
-                    # HOW CAN I COLLAPSE THIS INTO A FUNCION?
-                    #sync += self.write_reg(wr_addr, self.axi_lite.wdata, m)
-                    for r in self._regs:
-                        reg_name, reg_dir, reg_addr = r
-                        if reg_dir == 'rw':
-                            with m.If(wr_addr == reg_addr):
-                                sync += getattr(self.registers, reg_name).eq(self.axi_lite.wdata)
-                    #########################################################
                     m.next = "DONE"
             with m.State("WAITING_ADDR"):
                 comb += [self.axi_lite.awready.eq(1),
                          self.axi_lite.wready.eq(0),
-                         self.axi_lite.bvalid.eq(0),]
+                         self.axi_lite.bvalid.eq(0),
+                         we.eq(0),]
                 with m.If(self.axi_lite.aw_accepted()):
-                    #########################################################
-                    # HOW CAN I COLLAPSE THIS INTO A FUNCION?
-                    #sync += self.write_reg(self.axi_lite.awaddr, wr_data, m)
-                    for r in self._regs:
-                        reg_name, reg_dir, reg_addr = r
-                        if reg_dir == 'rw':
-                            with m.If(self.axi_lite.awaddr == reg_addr):
-                                sync += getattr(self.registers, reg_name).eq(wr_data)
-                    #########################################################
                     m.next = "DONE"
             with m.State("DONE"):
                 comb += [self.axi_lite.awready.eq(0),
                          self.axi_lite.wready.eq(0),
-                         self.axi_lite.bvalid.eq(1),]
+                         self.axi_lite.bvalid.eq(1),
+                         we.eq(1),]
                 with m.If(self.axi_lite.b_accepted()):
                     m.next = "IDLE"
 

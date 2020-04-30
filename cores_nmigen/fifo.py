9d9fec9d19283678ede028152c705254c2e63fce
akukulanski <akukulanski@gmail.com>
2020-04-29 23:30:42 -0300

    stream fifo CDC

--- cores_nmigen/fifo.py
+++ cores_nmigen/fifo.py
@@ -1,24 +1,20 @@
 from nmigen import *
-from nmigen.lib.fifo import SyncFIFOBuffered, SyncFIFO, AsyncFIFO
+from nmigen.lib.fifo import SyncFIFOBuffered, SyncFIFO, AsyncFIFOBuffered, AsyncFIFO
 
 
 class StreamFifo(Elaboratable):
 
-    def __init__(self, input_stream, output_stream, depth, fifo=SyncFIFOBuffered, domain='sync'):
+    def __init__(self, input_stream, output_stream, depth, fifo=SyncFIFOBuffered, *args, **kwargs):
+        assert input_stream._total_width == output_stream._total_width
         self.input = input_stream
         self.output = output_stream
-        assert self.input._total_width == self.output._total_width
-        self.width = self.input._total_width
-        self.depth = depth
-        self.fifo = fifo
-        self.domain = domain
+        self.fifo = fifo(width=input_stream._total_width, depth=depth, *args, **kwargs)
 
     def elaborate(self, platform):
         m = Module()
         comb = m.d.comb
-        sync = m.d[self.domain]
 
-        m.submodules.fifo_core = fifo = self.fifo(width=self.width, depth=self.depth)
+        m.submodules.fifo_core = fifo = self.fifo
 
         comb += fifo.w_data.eq(self.input._flat_data)
         comb += fifo.w_en.eq(self.input.accepted())
@@ -31,20 +27,9 @@ class StreamFifo(Elaboratable):
         return m
 
 
-if __name__ == '__main__':
+class StreamFifoCDC(StreamFifo):
 
-    fifo = StreamFifo(input_stream=DataStream(width, 'sink', name='input'),
-                      output_stream=DataStream(width, 'source', name='output'),
-                      depth=128)
-    ports = [fifo.input[f] for f in fifo.input.fields]
-    ports += [fifo.output[f] for f in fifo.output.fields]
-    main(fifo, ports=ports)
+    def __init__(self, input_stream, output_stream, depth, r_domain, w_domain, fifo=AsyncFIFO):
+        StreamFifo.__init__(self, input_stream, output_stream, depth, fifo=fifo,
+                            r_domain=r_domain, w_domain=w_domain)
 
-    # fifo_cdc = StreamFifoCDC(input_stream=DataStream(width, 'sink', name='input'),
-    #                          output_stream=DataStream(width, 'source', name='output'),
-    #                          depth=128,
-    #                          r_domain='sync_a',
-    #                          w_domain='sync_b')
-    # ports = [fifo_cdc.input[f] for f in fifo_cdc.input.fields]
-    # ports += [fifo_cdc.output[f] for f in fifo_cdc.output.fields]
-    # main(fifo_cdc, ports=ports)
--- cores_nmigen/test/test_fifo_cdc.py
+++ cores_nmigen/test/test_fifo_cdc.py
@@ -1,60 +1,70 @@
-# from nmigen_cocotb import run
-# from cores_nmigen.fifo import StreamFifoCDC
-# from cores_nmigen.test.interfaces import DataStreamDriver
-# import random
-# import pytest
-# import os
+from nmigen_cocotb import run
+from cores_nmigen.fifo import StreamFifoCDC
+from cores_nmigen.interfaces import DataStream
+from cores_nmigen.test.interfaces import DataStreamDriver
+import random
+import pytest
+import os
 
-# try:
-#     import cocotb
-#     from cocotb.triggers import RisingEdge
-#     from cocotb.clock import Clock
-#     from cocotb.regression import TestFactory as TF #renamed different than "Test*" so pytest won't show up a warning
-# except:
-#     pass
+try:
+    import cocotb
+    from cocotb.triggers import RisingEdge
+    from cocotb.clock import Clock
+    from cocotb.regression import TestFactory as TF #renamed different than "Test*" so pytest won't show up a warning
+except:
+    pass
 
+random.seed()
 
-# @cocotb.coroutine
-# def init_test(dut, period_ns_w, period_ns_r):
-#     dut.output__ready <= 0
-#     dut.input__valid <= 0
-#     dut.input__data <= 0
-#     dut.input__last <= 0
-#     dut.write_rst <= 1
-#     dut.read_rst <= 1
-#     cocotb.fork(Clock(dut.write_clk, period_ns_w, 'ns').start())
-#     cocotb.fork(Clock(dut.read_clk, period_ns_r, 'ns').start())
-#     yield RisingEdge(dut.write_clk)
-#     yield RisingEdge(dut.read_clk)
-#     dut.write_rst <= 0
-#     dut.read_rst <= 0
-#     yield RisingEdge(dut.write_clk)
-#     yield RisingEdge(dut.read_clk)
-#     yield RisingEdge(dut.write_clk)
-#     yield RisingEdge(dut.read_clk)
+@cocotb.coroutine
+def init_test(dut, period_ns_w, period_ns_r):
+    dut.output__ready <= 0
+    dut.input__valid <= 0
+    dut.input__data <= 0
+    dut.input__last <= 0
+    dut.write_rst <= 1
+    dut.read_rst <= 1
+    cocotb.fork(Clock(dut.write_clk, period_ns_w, 'ns').start())
+    cocotb.fork(Clock(dut.read_clk, period_ns_r, 'ns').start())
+    yield RisingEdge(dut.write_clk)
+    yield RisingEdge(dut.write_clk)
+    yield RisingEdge(dut.read_clk)
+    yield RisingEdge(dut.read_clk)
+    dut.write_rst <= 0
+    dut.read_rst <= 0
+    yield RisingEdge(dut.write_clk)
+    yield RisingEdge(dut.read_clk)
+    yield RisingEdge(dut.write_clk)
+    yield RisingEdge(dut.read_clk)
 
-# @cocotb.coroutine
-# def check_data(dut, period_ns_w, period_ns_r, burps_in, burps_out):
-#     length = 1000
-#     yield init_test(dut, period_ns_w, period_ns_r)
-#     input_stream = DataStreamDriver(dut, 'input_', dut.write_clk)
-#     output_stream = DataStreamDriver(dut, 'output_', dut.read_clk)
-#     data = [random.getrandbits(len(input_stream.bus.data)) for _ in range(length)]
-#     cocotb.fork(input_stream.send(data, burps=burps_in))
-#     rcv = yield output_stream.recv(burps=burps_out)
-#     assert data == rcv, f'\n{data}\n!=\n{rcv}'
-
-
-# tf_check = TF(check_data)
-# tf_check.add_option('period_ns_w', [10])
-# tf_check.add_option('period_ns_r', [10, 22, 3])
-# tf_check.add_option('burps_in',  [False, True])
-# tf_check.add_option('burps_out', [False, True])
-# tf_check.generate_tests(postfix='_cdc')
+@cocotb.coroutine
+def check_data(dut, period_ns_w, period_ns_r, burps_in, burps_out, dummy=0):
+    length = 200
+    yield init_test(dut, period_ns_w, period_ns_r)
+    input_stream = DataStreamDriver(dut, 'input_', dut.write_clk)
+    output_stream = DataStreamDriver(dut, 'output_', dut.read_clk)
+    data = [random.getrandbits(len(input_stream.bus.data)) for _ in range(length)]
+    cocotb.fork(input_stream.send(data, burps=burps_in))
+    rcv = yield output_stream.recv(burps=burps_out)
+    assert data == rcv, f'\n{data}\n!=\n{rcv}'
 
 
-# def test_main():
-#     fifo = StreamFifoCDC(random.randint(2, 20), random.randint(2, 10), r_domain='read', w_domain='write')
-#     ports = [fifo.input[f] for f in fifo.input.fields]   
-#     ports += [fifo.output[f] for f in fifo.output.fields]
-#     run(fifo, 'cores_nmigen.test.test_stream_fifo_cdc', ports=ports, vcd_file='stream_fifo_cdc.vcd')
+tf_check = TF(check_data)
+tf_check.add_option('period_ns_w', [10])
+tf_check.add_option('period_ns_r', [10, 22, 3])
+tf_check.add_option('burps_in',  [False, True])
+tf_check.add_option('burps_out', [False, True])
+tf_check.add_option('dummy', [0] * 3)
+tf_check.generate_tests(postfix='_cdc')
+
+
+@pytest.mark.parametrize("width, depth", [(random.randint(2, 20), random.randint(2, 10))])
+def test_main(width, depth):
+    fifo = StreamFifoCDC(input_stream=DataStream(width, 'sink', name='input'),
+                         output_stream=DataStream(width, 'source', name='output'),
+                         depth=depth,
+                         r_domain='read',
+                         w_domain='write')
+    ports = [fifo.input[f] for f in fifo.input.fields]   
+    ports += [fifo.output[f] for f in fifo.output.fields]
+    run(fifo, 'cores_nmigen.test.test_fifo_cdc', ports=ports, vcd_file='test_stream_fifo_cdc.vcd')
